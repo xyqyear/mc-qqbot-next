b06dfad0c28d8e11dce96ea5fe6199a1055beefe
xyqyear <xyqyear@gmail.com>
2024-10-28 15:49:17 +0800

    fix permission can't have matcher as arg; Because matcher is undetermined when checking permission.

--- mc_qqbot_next/plugins/mc_qqbot_next/permission.py
+++ mc_qqbot_next/plugins/mc_qqbot_next/permission.py
@@ -1,15 +1,13 @@
 from nonebot.adapters.onebot.v11 import GroupMessageEvent
 from nonebot.adapters.onebot.v11.bot import Bot
 from nonebot.adapters.onebot.v11.permission import GROUP_ADMIN, GROUP_OWNER
-from nonebot.matcher import Matcher
 
 group_admin_or_owner_permission = GROUP_ADMIN | GROUP_OWNER
 
 
-async def group_admin_or_owner(
-    matcher: Matcher, bot: Bot, msg: GroupMessageEvent
-) -> bool:
+async def group_admin_or_owner(bot: Bot, msg: GroupMessageEvent) -> bool:
     permission_passed = await group_admin_or_owner_permission(bot, msg)
     if not permission_passed:
-        await matcher.finish("权限不足")
+        await bot.send_group_msg(group_id=msg.group_id, message="权限不足")
+        return False
     return True
