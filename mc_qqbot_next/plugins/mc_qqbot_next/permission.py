728d006dea1762a595de06e6a6961d20003f3cf0
xyqyear <xyqyear@gmail.com>
2024-10-30 16:04:12 +0800

    it seems if we send message here, permission check will fail.

--- mc_qqbot_next/plugins/mc_qqbot_next/permission.py
+++ mc_qqbot_next/plugins/mc_qqbot_next/permission.py
@@ -5,9 +5,8 @@ from nonebot.adapters.onebot.v11.permission import GROUP_ADMIN, GROUP_OWNER
 group_admin_or_owner_permission = GROUP_ADMIN | GROUP_OWNER
 
 
-async def group_admin_or_owner(bot: Bot, msg: GroupMessageEvent) -> bool:
-    permission_passed = await group_admin_or_owner_permission(bot, msg)
+async def group_admin_or_owner(bot: Bot, event: GroupMessageEvent) -> bool:
+    permission_passed = await group_admin_or_owner_permission(bot, event)
     if not permission_passed:
-        await bot.send_group_msg(group_id=msg.group_id, message="权限不足")
         return False
     return True
