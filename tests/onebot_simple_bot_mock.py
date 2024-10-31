7ca412c48cc7ba841a456c74864a5f3bb7b87cab
xyqyear <xyqyear@gmail.com>
2024-10-31 14:56:56 +0800

    use a class for mock bot

--- tests/onebot_simple_bot_mock.py
+++ tests/onebot_simple_bot_mock.py
@@ -1,12 +1,17 @@
 from contextlib import contextmanager
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import AsyncMock, patch
+
+
+class MockBot:
+    def __init__(self):
+        self.send_group_msg = AsyncMock()
 
 
 @contextmanager
 def mock_server_to_group_bot():
+    mock_bot = MockBot()
     with patch(
         "mc_qqbot_next.plugins.mc_qqbot_next.server_to_group.get_onebot_bot",
-        new=MagicMock(),
-    ) as mock_bot:
-        mock_bot.send_group_msg = AsyncMock()
+        return_value=mock_bot,
+    ):
         yield mock_bot
