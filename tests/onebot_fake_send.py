319d7d4f4b0af594ac19933e896be5df70ce1556
xyqyear <xyqyear@gmail.com>
2024-10-31 13:08:38 +0800

    add more customizability to mock bot receive while preserving compatibility.

--- tests/onebot_fake_send.py
+++ tests/onebot_fake_send.py
@@ -6,15 +6,26 @@ from nonebot.matcher import Matcher
 from nonebug import App
 
 
-async def bot_should_respond(
-    app: App, matcher: type[Matcher], event: Event, mock_response: str | None = None
+async def bot_receive_event(
+    app: App,
+    matcher: type[Matcher],
+    event: Event,
+    mock_response: str | None = None,
+    should_pass_permission: bool = True,
+    should_pass_rule: bool = True,
 ):
     async with app.test_matcher(matcher) as ctx:
         adapter = nonebot.get_adapter(Onebot11Adapter)
         bot = ctx.create_bot(base=Onebot11Bot, adapter=adapter)
         ctx.receive_event(bot, event)
-        ctx.should_pass_permission(matcher)
-        ctx.should_pass_rule(matcher)
+        if should_pass_permission:
+            ctx.should_pass_permission(matcher)
+        else:
+            ctx.should_not_pass_permission(matcher)
+        if should_pass_rule:
+            ctx.should_pass_rule(matcher)
+        else:
+            ctx.should_not_pass_rule(matcher)
         if mock_response:
             ctx.should_call_send(event, mock_response, result=None)
             ctx.should_finished(matcher)
