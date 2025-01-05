ef73b819c42a4b15557b32d863a54b32cefed848
xyqyear <xyqyear@gmail.com>
2025-01-05 20:39:05 +0800

    update mc_excluded_servers type to support both list and string formats

--- mc_qqbot_next/plugins/mc_qqbot_next/config.py
+++ mc_qqbot_next/plugins/mc_qqbot_next/config.py
@@ -6,7 +6,7 @@ class Config(BaseModel):
     model_config = ConfigDict(extra="ignore")
 
     mc_servers_root_path: str = Field(None, validate_default=False)
-    mc_excluded_servers: list[str] = []
+    mc_excluded_servers: list[str] | str = []
     mc_default_server: str | None = None
     mc_group_id: int = Field(None, validate_default=False)
     mc_restart_wait_seconds: int = 60 * 10
@@ -22,3 +22,6 @@ if config.mc_default_server == "":
     config.mc_default_server = None
 if config.mc_group_id is None or config.mc_group_id == 0:
     raise ValueError("Please set MC_GROUP_ID")
+
+if isinstance(config.mc_excluded_servers, str):
+    config.mc_excluded_servers = config.mc_excluded_servers.split(",")
